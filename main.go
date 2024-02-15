bdd738174cc9ebe5dd29d8edc769ec4996bb3c31
farsa <puppetcooder@gmail.com>
2024-02-15 16:29:33 +0300

    add fan-in folder + basic code

--- main.go
+++ main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
 	fmt.Println("-------- Start pipeline PKG-------->")
+
+	gen := func() <-chan int {
+		ch := make(chan int)
+
+		go func() {
+			for i := 0; i < 20; i++ {
+				ch <- i
+			}
+			close(ch)
+		}()
+
+		return ch
+	}
+
+	ch1 := gen()
+	ch2 := gen()
+	ch3 := gen()
+	ch4 := gen()
+
+	fanin := func(chs ...<-chan int) <-chan int {
+		out := make(chan int)
+
+		go func() {
+			wg := sync.WaitGroup{}
+			wg.Add(len(chs))
+
+			for _, ch := range chs {
+				go func(ch <-chan int) {
+					for i := range ch {
+						out <- i
+					}
+					wg.Done()
+				}(ch)
+			}
+
+			wg.Wait()
+			close(out)
+		}()
+		return out
+	}
+
+	result := fanin(ch1, ch2, ch3, ch4)
+
+	for i := range result {
+		fmt.Println(i)
+	}
+
 }
